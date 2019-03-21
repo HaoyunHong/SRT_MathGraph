c77badb4169b2f509ff15f3ff5f011a228a98be0
谢韬 <2016011294@secoder.net>
2019-03-21 10:52:54 +0800

    新增节点中初始化条件：等式约束集合。更新题目求解示例程序。

--- graph_example.py
+++ graph_example.py
@@ -5,20 +5,20 @@ author: 谢韬
 
 from node import *
 
-graph_description = '''
+#graph_description = '''
 Real = ObjectNode("Real", "x", "", toSympy="self['x']")
 Complex = ObjectNode("Complex", "a b", "", toSympy="self['a']+self['b']*I")
-GetRePart = OperationNode("GetRePart", [Complex], [Real], "self.output[0]['x']=self.input[0]['a']")
-GetImPart = OperationNode("GetImPart", [Complex], [Real], "self.output[0]['x']=self.input[0]['b']")
-CalcMod = OperationNode("CalcMod", [Complex], [Real], "self.output[0]['x']=sqrt(self.input[0]['a']**2+self.input[0]['b']**2)")
-ComplexConjugate = ConstraintNode("ComplexConjugate", [Complex, Complex], "self.input[0]['a']==self.input[1]['a']&&self.input[0]['b']=-self.input[1]['b']")
-'''
+GetRePart = OperationNode("GetRePart", [Complex], [Real], "self.output[0]['x']=self.input[0]['a']", E=["self.output[0]['x']-self.input[0]['a']"])
+GetImPart = OperationNode("GetImPart", [Complex], [Real], "self.output[0]['x']=self.input[0]['b']", E=["self.output[0]['x']-self.input[0]['b']"])
+CalcMod = OperationNode("CalcMod", [Complex], [Real], "self.output[0]['x']=sqrt(self.input[0]['a']**2+self.input[0]['b']**2)", E=["self.output[0]['x']-sqrt(self.input[0]['a']**2+self.input[0]['b']**2)"])
+ComplexConjugate = ConstraintNode("ComplexConjugate", [Complex, Complex], "self.input[0]['a']==self.input[1]['a']&&self.input[0]['b']=-self.input[1]['b']", E=["self.input[0]['a']-self.input[1]['a']", "self.input[0]['a']+self.input[1]['b']"])
+#'''
 
-exec(graph_description)
+#exec(graph_description)
 
 def createObjectInstance(objNode, name):
     exec("%s = ObjectInstance(%s, '%s')" % (name, objNode, name), globals())
-
+"""
 #题目1：
 #z1和z2是复数，z1=z2*i，z1+z2=-1+7i，求z1的模长r
 
@@ -76,6 +76,71 @@ for name in instances:
     instances[name].subs(res)
 print("结果：")
 print(r['x'])
+"""
+#题目2：和题目1相同，不过计算方法不同
+#这里使用了Instnace的新特性：提取等式约束
+#这需要在定义中设置E项
+#z1和z2是复数，z1=z2*i，z1+z2=-1+7i，求z1的模长r
+
+#定义解题用列表
+ins_list = []
+constraints = []
+tosolve = []
+
+#以下部分因题而异
+#定义z1 z2 r
+z1 = ObjectInstance(Complex, "z1")
+ins_list.append(z1)
+z2 = ObjectInstance(Complex, "z2")
+ins_list.append(z2)
+r = ObjectInstance(Real, "r")
+ins_list.append(r)
+#约束：两个等式
+constraints.append("z1-z2*I")
+constraints.append("z1+z2+1-7*I")
+#约束，r是z1的模长
+op = OperationInstance(CalcMod, [z1], [r])
+ins_list.append(op)
+
+#以下部分不随题目改变而改变
+#转化约束：现有约束为题目给出的明文约束，其中包含数学实体
+print("约束集合（包含数学实体）：")
+for i in range(0, len(constraints)):
+    #替换数学实体为sympy对象
+    for name in instances:
+        if instances[name].objNode.isSympyInstance():
+            constraints[i] = constraints[i].replace(name, name + ".toSympyInstance()")
+    print(constraints[i])
+print("约束集合（只含基本属性）：")
+for i in range(0, len(constraints)):
+    constraints[i] = eval(constraints[i])
+    print(constraints[i])
+#完善约束，将诸多节点定义的约束添加到约束集中
+for ins in ins_list:
+    constraints = constraints + ins.getEquationSet()
+print("最终约束集合：")
+print(constraints)
+#查找所有要解的symbol
+for name in instances:
+    #搜索所有要解的symbol
+    #print(instances[name].getSymbols())
+    tosolve = tosolve + (instances[name].getSymbols())
+print("所有的基本属性：")
+print(tosolve)
+results = solve(constraints, tosolve, dict=True)
+print("所有基本属性的解：")
+print(results)
+
+#解约束，以下部分因题而异（因为最终结果不定）
+for result in results:
+    for name in instances:
+        #将instance中的基本属性替换为解
+        instances[name].subs(result)
+    print("结果：")
+    print(r['x'])
+
+
+
 
 
 
--- node.py
+++ node.py
@@ -14,17 +14,19 @@ class Instance:
 instances = {}
 
 class ObjectNode(Node):
-    def __init__(self, name, P, C, toSympy=""):
+    def __init__(self, name, P, C, toSympy="", E=[]):
         #name：指本ObjectNode的名称，例如"Complex"，"Triangle"等
         #P：指对象节点的属性，格式为若干个名称用空格隔开，例如"a b c"
         #C：指对象节点的约束，对某一属性的访问用"self[属性]"表示
         #   例如三角形两边之和大于第三边可以是"self['a']+self['b']>self['c']"
         #toSympy：默认为空字符串，详细可见ObjectInstance类中的toSympyInstance方法
+        #E：约束等式的集合，例如三角形有["self['A']+self['B']+self['C']-pi"]等
         self.name = name
         self.P = P.split(" ")
         if self.P.count("") > 0:
             self.P.remove("")
         self.C = C
+        self.E = E
         self.toSympy = toSympy
         self.instances = {}
         '''
@@ -36,16 +38,18 @@ class ObjectNode(Node):
         return self.toSympy != ""
 
 class OperationNode(Node):
-    def __init__(self, name, input, output, f):
+    def __init__(self, name, input, output, f, E=[]):
         #name：指本OperationNode的名称，例如"CalcMod"等
         #input：一个list，内含若干对象节点，代表输入的类型，例如[Complex]
         #output：一个list，内含若干对象节点，代表输出的类型，例如[Real]
         #f：代表运算规则，其中对输入的访问为"self.input[index][基本属性]"，输出同理
         #   例如复数模长的计算可以是"self.output[0]['x']=sqrt(self.input[0]['a']**2+self.input[0]['b']**2)"
+        #E：约束等式的集合，例如复数模长有["self.output[0]['x']-sqrt(self.input[0]['a']**2+self.input[0]['b']**2)"]等
         self.name = name
         self.input = input
         self.output = output
         self.f = f
+        self.E = E
         '''
         self.instances = []
         self.toSave = "OperationNode\n" + name + "\n"
@@ -66,14 +70,16 @@ class OperationNode(Node):
         '''
 
 class ConstraintNode(Node):
-    def __init__(self, name, input, C):
+    def __init__(self, name, input, C, E=[]):
         #name：指本ConstraintNode的名称，例如"ComplexConjugate"等
         #input：一个list，内含若干对象节点，代表输入的类型，例如[Complex, Complex]等
         #C：约束字符串，代表这个点的约束条件，使用self.input[index][基本属性]来对属性进行访问
         #   例如共轭的约束字符串可以是"self.input[0]['a']==self.input[1]['a'] && self.input[0]['b']==-self.input[1]['b']"
+        #E：约束等式的集合，例如复数共轭有["self.input[0]['a']-self.input[1]['a']", "self.output[0]['b']+self.output[1]['b']"]等
         self.name = name
         self.input = input
         self.C = C
+        self.E = E
         '''
         self.instances = []
         self.toSave = "ConstraintNode\n" + name + "\n"
@@ -145,6 +151,12 @@ class ObjectInstance(Instance):
                 ret.append(self.PV[p])
         return ret
 
+    def getEquationSet(self):
+        ret = []
+        for e in self.objNode.E:
+            ret.append(eval(e))
+        return ret
+
     def __getitem__(self, item):
         return self.PV[item]
 
@@ -160,6 +172,12 @@ class OperationInstance(Instance):
     def calculate(self):
         exec(self.opNode.f)
 
+    def getEquationSet(self):
+        ret = []
+        for e in self.opNode.E:
+            ret.append(eval(e))
+        return ret
+
 class ConstraintInstance(Instance):
     def __init__(self, conNode, input):
         self.conNode = conNode
@@ -167,6 +185,12 @@ class ConstraintInstance(Instance):
 
     def isLegal(self):
         return eval(self.conNode.C)
+
+    def getEquationSet(self):
+        ret = []
+        for e in self.conNode.E:
+            ret.append(eval(e))
+        return ret
 
 
 
